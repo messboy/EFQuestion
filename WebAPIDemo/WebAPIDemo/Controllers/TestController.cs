6c8eb3fdef78d7d0124f4da4a1b2a4e31f43e47f
andy <dreamrrrr@hotmail.com>
2015-11-09 22:48:25 +0800

    問題 : ICollection<Category> 對應因為沒有mapping到PrimaryKey會報錯,請問該如何調整 ps.Category 的資料可能資料庫以經存在也可能沒有需另外新增

---
+++ WebAPIDemo/WebAPIDemo/Controllers/TestController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIDemo.Models;
+
+using System.Data.Entity;
+
+namespace WebAPIDemo.Controllers
+{
+    public class TestController : ApiController
+    {
+        
+
+        public HttpResponseMessage Update(ViewModel viewModel)
+        {
+            try
+            {
+                using (TestEntities db = new TestEntities())
+                {
+                    var model = db.Article.Find(viewModel.ArticleID);
+
+                    //mapping
+                    Mapper.CreateMap<ViewModel, Article>()
+                    .ForMember(i => i.ArticleID, s => s.MapFrom(i => i.ArticleID))
+                    .ForMember(i => i.Name, s => s.MapFrom(i => i.Name))
+                    .ForMember(i => i.Category, s => s.MapFrom(i => i.Category));
+                    Mapper.Map(viewModel, model);
+
+                    //問題 : ICollection<Category> 對應因為沒有mapping到PrimaryKey會報錯,請問該如何調整
+                    // ps.Category 的資料可能資料庫以經存在也可能沒有需另外新增
+                    
+                    db.SaveChanges();
+
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, true);
+
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message); ;
+            }
+        }
+
+
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+
+                List<Article> model;
+                using (TestEntities db = new TestEntities())
+                {
+                    model = db.Article.Include(a => a.Category).ToList();
+
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, model);
+
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message); ;
+            }
+        }
+    }
+}
---
+++ WebAPIDemo/WebAPIDemo/Models/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIDemo.Models
+{
+    public class ViewModel
+    {
+        public string ArticleID { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Category> Category { get; set; }
+
+    }
+}
